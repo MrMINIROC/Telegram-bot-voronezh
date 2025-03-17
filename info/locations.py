157141903c4e20d144bfa390eedb2f6c72eecfae
minroc_laptop <root@miniroc.pc>
2025-03-17 17:42:01 +0300

    Обновлен файл locations.py

--- info/locations.py
+++ info/locations.py
@@ -2,32 +2,139 @@
 locations = [
     {
         "name": "Вокзал Воронеж-1",
-        "description": "Воронежский вокзал — это главный железнодорожный вокзал города Воронежа.",
-        "photo": "https://avatars.mds.yandex.net/get-altay/771751/2a0000018846d830dc951971b2b553e500ad/orig",
-        "url": "yandex.ru/maps/193/voronezh/?ll=39.211272%2C51.665689&mode=routes&rtext=~51.679694%2C39.206252&rtt=pd&ruri=~ymapsbm1%3A%2F%2Ftransit%2Fstop%3Fid%3Dstation__lh_2014001&z=14.6"
+        "description": "Вокзал Воронеж-1 — главный железнодорожный вокзал города Воронеж.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
     },
     {
         "name": "Кафедральный собор Благовещенский",
-        "description": "Кафедральный собор Благовещенский лалала",
-        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/%D0%91%D0%BB%D0%B0%D0%B3%D0%BE%D0%B2%D0%B5%D1%89%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B0%D1%84%D0%B5%D0%B4%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D1%81%D0%BE%D0%B1%D0%BE%D1%80.jpg/1280px-%D0%91%D0%BB%D0%B0%D0%B3%D0%BE%D0%B2%D0%B5%D1%89%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B0%D1%84%D0%B5%D0%B4%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D1%81%D0%BE%D0%B1%D0%BE%D1%80.jpg",
-        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.211272%2C51.665689&mode=routes&rtext=~51.676129%2C39.210565&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1372852636&z=14"
+        "description": "Кафедральный собор Благовещенский — главный храм Воронежа, известный своей архитектурой.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
     },
     {
         "name": "Парк Орлёнок",
-        "description": "СУПЕР ПАРК",
-        "photo": "https://tv-gubernia.ru/images/cms/thumbs/4f6b179e0e946b220960ff3a05bc399889690ff8/dsc_3558_800_auto_jpg.jpg",
-        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.196977%2C51.674859&mode=routes&rtext=~51.674938%2C39.206391&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1039969780&z=15.95"
+        "description": "Парк Орлёнок — популярное место отдыха с зелеными аллеями и детскими площадками.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
     },
     {
         "name": "Петровский сквер",
-        "description": "НУ СКВЕР А ЧО",
-        "photo": "https://2vrn.ru/wp-content/uploads/2018/04/petrovskij-skver-voronezh.jpg",
-        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673789%2C39.211687&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D51877151068&z=15.86"
+        "description": "Петровский сквер — уютное место для прогулок и отдыха в центре города.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
     },
     {
-        "name": "Музей Крамского",
-        "description": "Музей имени Крамского — это художественный музей с богатой коллекцией.",
-        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/bezymyannyy_1.png",
+        "name": "Воронежский областной художественный музей имени И.Н. Крамского",
+        "description": "Воронежский областной художественный музей — один из крупнейших культурных центров Воронежа.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
         "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Стела в честь 70-летия Победы в Великой Отечественной войне",
+        "description": "Стела, посвященная 70-летию Победы, является символом памяти и уважения к ветеранам.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Памятник Белому Биму Черное Ухо",
+        "description": "Памятник Белому Биму — символ любви и верности, установленный в честь известного литературного героя.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Театр кукол",
+        "description": "Театр кукол — место, где проходят увлекательные спектакли для детей и взрослых.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Кольцовский сквер",
+        "description": "Кольцовский сквер — зеленая зона для отдыха и прогулок в центре города.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Воронежский государственный театр оперы и балета",
+        "description": "Театр оперы и балета — культурный центр, где проходят спектакли и концерты.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Площадь Ленина",
+        "description": "Площадь Ленина — центральная площадь города, место проведения мероприятий.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Каменный мост",
+        "description": "Каменный мост — исторический мост, соединяющий берега реки Воронеж.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Церковь",
+        "description": "Церковь — место для молитвы и духовного общения.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+        "name": "Советская площадь",
+        "description": "Советская площадь — историческая площадь, окруженная памятниками архитектуры.",
+        "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+        "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51. }"},
+    {
+    "name": "Воронежское водохранилище",
+    "description": "Воронежское водохранилище — это крупный водоем, который является важной частью экосистемы региона и популярным местом для отдыха.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+    "name": "Адмиралтейская площадь",
+    "description": "Адмиралтейская площадь — это историческая площадь в центре Воронежа, известная своими архитектурными памятниками.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+    "name": "Корабль-музей “Гото Предестинация”",
+    "description": "Корабль-музей “Гото Предестинация” — это действующая историческая копия первого линейного корабля Российского военно-морского флота.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+    "name": "Бульвар Чернавская Дамба",
+    "description": "Бульвар Чернавская Дамба — это живописное место для прогулок и отдыха на берегу водохранилища.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+    "name": "Дельфин",
+    "description": "Дельфин — это аквапарк и развлекательный комплекс, предлагающий множество водных аттракционов.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+    "name": "Стадион Динамо",
+    "description": "Стадион Динамо — это спортивная арена, где проводятся футбольные матчи и другие спортивные мероприятия.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
+    },
+    {
+    "name": "Центральный парк",
+    "description": "Центральный парк — это популярное место для отдыха, прогулок и активного времяпрепровождения.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.240000%2C51.710000&z=12"
+    },
+    {
+    "name": "Центральный парк",
+    "description": "Центральный парк — это популярное место для отдыха, прогулок и активного времяпрепровождения.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.240000%2C51.710000&z=12"
+    },
+    {
+    "name": "Бринкманский сад",
+    "description": "Бринкманский сад — это уютный зеленый уголок в городе, идеальный для спокойных прогулок.",
+    "photo": "https://www.okrae.odbvrn.ru/sites/okrae.odbvrn.ru/files/usersfiles/vokzal_0.jpg",
+    "url": "https://yandex.ru/maps/193/voronezh/?ll=39.197812%2C51.674859&mode=routes&rtext=~51.673911%2C39.209867&rtt=pd&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D1124831870&z=15.86"
     },
 ]
